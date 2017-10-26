5fd3e5578b03578836fe991e623a269fdc3b186c
James McMillan <jmcmillan29@gmail.com>
2017-10-26 11:31:23 -0700

    updated my game.js file with some more detailed comments for almost every task

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,21 +3,24 @@
 // Week 4 Homework Assignment
 
 // Variables
-let wins = 0;
-let losses = 0;
-let counter = 0;
-let magicNumber = 0;
+let wins = 0; // how many times the user has hit the magicNumber
+let losses = 0; // how many times the user has not hit the magicNumber
+let counter = 0; // track the number the user is generating by clicking on the cards
+let magicNumber = 0; // target number that the user is trying to hit
 
 // Function to Generate a Random Number between 19-120 for the MagicNumber
 var magicNumberGenerator = function() {
 	magicNumber = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
 	// console.log(magicNumber);
+	// update the magic-number h1 with the new magicNumber for the round
 	$('#magic-number').text(magicNumber);
 };
 
 // Function to Generate a Random Number betweent 1-12 for each Cards assigned value
 var cardValueRandomNumberGenerator = function() {
+	// for loop to assign 4 different random values for each card
 	for (var i = 1; i < 5; i++) {
+		// Apply 4 different random values to each of the card images
 		$('#card-'+[i]).val( Math.floor(Math.random() * (12 - 1 + 1)) + 1 );
 	}
 
@@ -26,11 +29,16 @@ var cardValueRandomNumberGenerator = function() {
 // On Click function for each card
 
 $('.card').on('click', function() {
+	// assign cardValue to the value that was assigned in the cardValueRandomNumberGenerator function
 	var cardValue = this.value;
+	// convert the cardValue into a Number
 	cardValue = Number(cardValue);
 	// console.log(cardValue);
+	// add the cardValue to the counter
 	counter += cardValue;
+	// update the counter with the addition of the cardValue
 	$('#user-number').text(counter);
+	// condition for wins and losses
 	if (counter === magicNumber) {
 		alertWin();
 	} else if ( counter >= magicNumber ) {
@@ -65,7 +73,6 @@ var resetGame = function() {
 };
 
 // Update The Stats
-
 var updateStats = function() {
 	var statsHtml = 
 		"<ul>" +
